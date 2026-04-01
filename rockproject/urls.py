2b1decc6b85db67a2beb1dfde57dc42b956db75b
Larissa Ferreira <larissadferr@gmail.com>
2026-04-01 15:45:12 -0500

    added type view

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rockapi.views import register_user, login_user
+from rockapi.views import register_user, login_user, TypeView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"types", TypeView, "type")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register', register_user),
-    path('login', login_user),
-    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("register", register_user),
+    path("login", login_user),
+    path("admin/", admin.site.urls),
 ]
